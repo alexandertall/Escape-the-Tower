a9ddf9ff655d0acfc33c29097d8f982f3344db18
Lazyfella <alexander.tallgren@gmail.com>
2022-12-17 23:51:22 +0200

    Dropping box on platform makes platform to shrink and change color

--- Assets/_Scripts/TriggerPlatform.cs
+++ Assets/_Scripts/TriggerPlatform.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class TriggerPlatform : MonoBehaviour
 {
-
+    [SerializeField] public float shrinkAmount = 0.5f;
     [SerializeField] public Rigidbody objectRigidbody;
+
+    public Material newMaterial;
+
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Puzzle_Object" && objectRigidbody.useGravity == true)
         {
-            Debug.Log("Works");
+            transform.localScale = new Vector3(1, shrinkAmount, 1);
+
+            Renderer rend = GetComponent<Renderer>();
+            rend.material = new Material(Shader.Find("Specular"));
+
         }
     }
 }
