87a51bd00fe8465aec18fabe53234bb23636ca32
Lazyfella <alexander.tallgren@gmail.com>
2023-01-15 10:27:38 +0200

    Pickup item glows then held in hand

--- Assets/_Scripts/ObjectGrabbable.cs
+++ Assets/_Scripts/ObjectGrabbable.cs
@@ -7,21 +7,27 @@ public class ObjectGrabbable : MonoBehaviour
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
 
+    public Material originalMaterial;
+    public Material highlightMaterial;
+
     private void Awake()
     {
         objectRigidbody = GetComponent<Rigidbody>();
+        originalMaterial = GetComponent<Renderer>().material;
     }
 
     public void Grab(Transform objectGrabPointTransform)
     {
         this.objectGrabPointTransform = objectGrabPointTransform;
         objectRigidbody.useGravity = false;
+        GetComponent<Renderer>().material = highlightMaterial;
     }
 
     public void Drop()
     {
         this.objectGrabPointTransform = null;
         objectRigidbody.useGravity = true;
+        GetComponent<Renderer>().material = originalMaterial;
     }
 
     private void FixedUpdate()
